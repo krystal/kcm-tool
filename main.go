cc94bf96c30ea1a06688483ceb8614b072e8a6db
Adam Cooke <adam@adam.ac>
2022-07-21 18:08:49 +0100

    feat: support for --version

--- main.go
+++ main.go
@@ -8,10 +8,18 @@ import (
 	flag "github.com/spf13/pflag"
 )
 
+var version = "dev"
+
 func main() {
 
-	var path *string = flag.String("config", "/etc/kcm.yaml", "Path to the config file")
+	var path *string = flag.StringP("config", "c", "/etc/kcm.yaml", "Path to the config file")
+	var versionFlag *bool = flag.BoolP("version", "v", false, "Print the version")
 	flag.Parse()
+
+	if *versionFlag {
+		println(version)
+		os.Exit(0)
+	}
 
 	output := zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}
 	logger := zerolog.New(output).With().Timestamp().Logger()
